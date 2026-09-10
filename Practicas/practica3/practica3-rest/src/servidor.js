3495c51be9d1a801587acc20fdf234e1f3ccaef8
dominique01Cruz <dominiquecg01@gmail.com>
2026-09-10 14:54:15 -0400

    actualizacion del word

---
+++ Practicas/practica3/practica3-rest/src/servidor.js
@@ -0,0 +1,16 @@
+﻿const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./esquema");
+const resolvers = require("./resolvers");
+const { crearCargadores } = require("./cargadores");
+
+async function main() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async () => ({ cargadores: crearCargadores() }),
+  });
+  console.log("GraphQL escuchando en " + url);
+}
+
+main();
